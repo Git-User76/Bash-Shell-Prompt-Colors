2e3ca35b23072155972ba305800c482023c709e0
BortSimpson <151472473+Git-User76@users.noreply.github.com>
2025-09-02 20:23:57 +0000

    Update prompt-color.sh

--- prompt-color.sh
+++ prompt-color.sh
@@ -1,5 +1,5 @@
-# Main: green color
-export PS1='\[\e[0;32m\][\u@\h \W]\$\[\e[0m\] '
+# Root prompt - RED theme
+export PS1='\[\033[31m\]\u@\h\[\033[0m\] \[\033[34m\]\w\[\033[0m\] \[\033[31m\]❯\[\033[0m\] '
 
-# Remote: red color
-export PS1='\[\e[0;91m\][\u@\h \W]\$\[\e[0m\] '
+# Regular user prompt - CYAN/GREEN theme
+export PS1='\[\033[36m\]\u@\h\[\033[0m\] \[\033[34m\]\w\[\033[0m\] \[\033[32m\]❯\[\033[0m\] '
