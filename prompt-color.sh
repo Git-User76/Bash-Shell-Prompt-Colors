3d9a28e0d7432a6350d7a8f2aeb5e99d0af3362f
Daniel <151472473+Git-User76@users.noreply.github.com>
2025-09-29 14:16:14 +0000

    Deleted sample prompt-color.sh
    - Deleted sample prompt-color.sh script. 
    - There is no need for the separate script file. Everything is in README.md

--- prompt-color.sh
+++
@@ -1,5 +0,0 @@
-# Root prompt - RED theme
-export PS1='\[\033[31m\]\u@\h\[\033[0m\] \[\033[34m\]\w\[\033[0m\] \[\033[31m\]❯\[\033[0m\] '
-
-# Regular user prompt - CYAN/GREEN theme
-export PS1='\[\033[36m\]\u@\h\[\033[0m\] \[\033[34m\]\w\[\033[0m\] \[\033[32m\]❯\[\033[0m\] '
